f2201ba9fa7037334bbfd25600263685771b3eae
Artyom Palvelev <artyompp@google.com>
2023-02-21 16:22:50 +0000

    gamesdk: optimize touch events handling by GameActivity
    We now pass as much information as possible from Java Activity,
    so that we can do as few JNI calls as possible. Note that touch events
    can happen often and thus their handling can be a bottleneck.
    
    Bug: 247548747
    Test: build and run AGDKTunnel
    Change-Id: I7e21e768f75010656d4bca9792b0a07657924078

--- android-activity/game-activity-csrc/game-activity/GameActivity.cpp
+++ android-activity/game-activity-csrc/game-activity/GameActivity.cpp
@@ -845,13 +845,22 @@ extern "C" int GameActivity_getUIMode(GameActivity *) {
 }
 
 static bool onTouchEvent_native(JNIEnv *env, jobject javaGameActivity,
-                                jlong handle, jobject motionEvent) {
+                                jlong handle, jobject motionEvent,
+                                int pointerCount, int historySize, int deviceId,
+                                int source, int action, int64_t eventTime,
+                                int64_t downTime, int flags, int metaState,
+                                int actionButton, int buttonState,
+                                int classification, int edgeFlags,
+                                float precisionX, float precisionY) {
     if (handle == 0) return false;
     NativeCode *code = (NativeCode *)handle;
     if (code->callbacks.onTouchEvent == nullptr) return false;
 
     static GameActivityMotionEvent c_event;
-    GameActivityMotionEvent_fromJava(env, motionEvent, &c_event);
+    GameActivityMotionEvent_fromJava(
+        env, motionEvent, &c_event, pointerCount, historySize, deviceId, source,
+        action, eventTime, downTime, flags, metaState, actionButton,
+        buttonState, classification, edgeFlags, precisionX, precisionY);
     return code->callbacks.onTouchEvent(code, &c_event);
 
 }
@@ -971,7 +980,7 @@ static const JNINativeMethod g_methods[] = {
     {"onSurfaceRedrawNeededNative", "(JLandroid/view/Surface;)V",
      (void *)onSurfaceRedrawNeeded_native},
     {"onSurfaceDestroyedNative", "(J)V", (void *)onSurfaceDestroyed_native},
-    {"onTouchEventNative", "(JLandroid/view/MotionEvent;)Z",
+    {"onTouchEventNative", "(JLandroid/view/MotionEvent;IIIIIJJIIIIIIFF)Z",
      (void *)onTouchEvent_native},
     {"onKeyDownNative", "(JLandroid/view/KeyEvent;)Z",
      (void *)onKeyDown_native},
@@ -1126,6 +1135,9 @@ extern "C" int GameActivity_register(JNIEnv *env) {
                              windowInsetsCompatType_class, methodNames[i],
                              "()I");
     }
+
+    GameActivityEventsInit(env);
+
     return jniRegisterNativeMethods(env, kGameActivityPathName, g_methods,
                                     NELEM(g_methods));
 }
